29a5c5e2cad257019b506f548f9a86c9af4830d6
SriFromKoding <mypmnode@gmail.com>
2015-07-09 09:50:04 +0000

    first commit

---
+++ public/modules/core/controllers/header.client.controller.js
@@ -0,0 +1,21 @@
+'use strict';
+
+angular.module('core').controller('HeaderController', ['$scope', 'Authentication', 'Menus',
+	function($scope, Authentication, Menus) {
+		$scope.authentication = Authentication;
+		$scope.isCollapsed = false;
+		$scope.isVCollapsed = true;
+		$scope.isVPCollapsed = true;
+		$scope.isPCollapsed = true;
+		$scope.menu = Menus.getMenu('topbar');
+
+		$scope.toggleCollapsibleMenu = function() {
+			$scope.isCollapsed = !$scope.isCollapsed;
+		};
+
+		// Collapsing the menu after navigation
+		$scope.$on('$stateChangeSuccess', function() {
+			$scope.isCollapsed = true;
+		});
+	}
+]);
